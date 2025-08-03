da84d282a7deba7cb10d34ad83f68b7e0cbdb08f
Scott Talbot <s@chikachow.org>
2025-08-03 15:44:05 +1000

    feat(census-management-go): create skeleton

---
+++ internal/census-management-go/census_management.go
@@ -0,0 +1,9 @@
+package censusmanagement
+
+const (
+	// DefaultBaseURL is the default base URL of the Census API.
+	DefaultBaseURL = "https://app.getcensus.com"
+
+	// DefaultUserAgent is the default user agent.
+	DefaultUserAgent = "census-management-go/0.1"
+)
---
+++ internal/census-management-go/ogen.go
@@ -0,0 +1,3 @@
+package censusmanagement
+
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen -target . -package censusmanagement -clean openapi.yml
